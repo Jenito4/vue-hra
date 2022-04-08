a1606539b1c381fe64122b6a6447d4ecf982da2c
Jan Mašlej <102227021+Jenito4@users.noreply.github.com>
2022-04-08 14:38:58 +0200

    Add files via upload

---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+    <p>Reaction time - {{score}} ms</p>
+    <p class="rank">{{rank}}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data(){
+        return {
+            rank: null
+        }
+    },
+    mounted(){
+        if (this.score < 250) {
+            this.rank = 'Woooow!'
+        } else if (this.score < 400) {
+            this.rank = 'rocket'
+        } else {
+            this.rank = 'you suck'
+        }
+    }
+}
+</script>
+
+<style>
+    .rank {
+        font-size: 1.4 em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
